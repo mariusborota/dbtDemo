fd1f8810e00a5d813e4200f33201f8ffb682081a
Marius Borota <marius.borota@sag-ag.ch>
2023-12-26 17:27:11 +0000

    after adding grant_select macro

---
+++ macros/grant_select.sql
@@ -0,0 +1,16 @@
+{% macro grant_select(pSchema = target.schema, pRole = target.role) %}
+
+    {% set sql %}
+        grant usage on schema {{ pSchema }} to role {{ pRole }};
+        grant select on all tables in schema {{ pSchema }} to role {{ pRole }};
+        grant select on all views in schema {{ pSchema }} to role {{ pRole }};
+    {% endset %}
+
+
+    
+    {{ log('Granting select on all tables and views in schema ' ~ target.schema ~ ' to role ' ~ role ~ ' ' ~ sql, info=True) }}
+    {#
+     {% do run_query(sql) %}
+    {{ log('Privileges granted', info=True) }}
+    #}
+{% endmacro %}
